b38c4056eb6929c17be881c6bfd357365f9490e5
KingsOg <nft33@protonmail.com>
2022-09-21 13:46:41 +0100

    two sum problem and answer

---
+++ twoSum.js
@@ -0,0 +1,36 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+var twoSum = function (nums, target) {
+    // Array to store the result
+    result = [];
+    // Map to store the difference and its index
+    index_map = new Map();
+    // Loop for each element in the array
+    for (let i = 0; i < nums.length; i++) {
+        let difference = target - nums[i];
+        if (index_map.has(difference)) {
+            result[0] = i;
+            result[1] = index_map.get(difference);
+            break;
+        } else {
+            index_map.set(nums[i], i);
+        }
+    }
+    return result;
+};
+
+// OR
+
+var twoSum = function(nums, target) {
+    for(var i=0;i<nums.length;i++){
+        for(var j = i+1;j<nums.length;j++){
+            if(nums[i]+nums[j] == target){
+                return [i,j]
+            }
+        }
+    }
+};
